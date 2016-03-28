789ff3c8fe6d61621a29cc5d9fd13cbb52ef8340
Moririn27 <leoflash21@gmail.com>
2016-03-28 17:22:21 -0430

    bullshit

--- app/src/main/java/com/example/isabel/chatwhatsapp/ChatArrayAdapter.java
+++ app/src/main/java/com/example/isabel/chatwhatsapp/ChatArrayAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -18,7 +17,8 @@ import java.util.regex.Pattern;
  */
 public class ChatArrayAdapter extends ArrayAdapter<MainActivity.ChatMessage> {
     private TextView chatText;
-    private List<MainActivity.ChatMessage> chatMessageList = new ArrayList<MainActivity.ChatMessage>();
+    private List<MainActivity.ChatMessage>
+            chatMessageList = new ArrayList<MainActivity.ChatMessage>();
     private Context context;
 
     @Override
